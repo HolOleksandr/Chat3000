bed636ebd4b736657c010b27b4f9575f701a2954
Oleksandr Holovachuk <golovachuk.alex@gmail.com>
2023-05-05 16:33:02 +0300

    Crashed

--- Chat.Blazor.Server/Validators/CreateGroupValidator.cs
+++ Chat.Blazor.Server/Validators/CreateGroupValidator.cs
@@ -10,7 +10,7 @@ namespace Chat.Blazor.Server.Validators
         {
             RuleFor(x => x.Name)
                  .NotEmpty().WithMessage("Group name is required")
-                 .Length(2, 10).WithMessage("Group name must be between 2-10 characters.");
+                 .Length(2, 15).WithMessage("Group name must be between 2-10 characters.");
 
             RuleFor(x => x.Description)
                  .NotEmpty().WithMessage("Description is required")
